488b2b5c7568687d23b9f5f181d1e615501dab90
willou <walid.boubguira@gmail.com>
2018-11-27 16:57:50 -0500

    ss

--- dev/test.js
+++ dev/test.js
@@ -2,4 +2,27 @@ const Blockchain = require('./blockchain');
 
 const bitCoin = new Blockchain()
 
-bitCoin.hashBlock()
+const previousBlockHash = '05das5das5d5asd5';
+const currentBlockData = [
+    {
+        amount: 14,
+        sender: "sdfdsfsdf445sd45fsd",
+        recipient: "sfdsf44455sd5f5sd4d2d2"
+    },
+    {
+        amount: 154,
+        sender: "sdfdsfsdf445sd45fsd",
+        recipient: "sfdsf5sd5f5sd4d2d2"
+    },
+    {
+        amount: 204,
+        sender: "sdfdsfsdf445sd45fsd",
+        recipient: "sfdsf5sd5f5sd4d2d2"
+    },
+];
+
+console.log(bitCoin.hashBlock(previousBlockHash, currentBlockData, 8893));
+
+
+
+// console.log(bitCoin.hashBlock("dsadasdasdasd", 222, "555555555555555555"))
