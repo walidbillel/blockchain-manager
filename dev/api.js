40a5abd7fad42101a7559b6e1dbc42955e7d6534
willou <walid.boubguira@gmail.com>
2018-11-29 12:37:24 -0500

    issue

--- dev/api.js
+++ dev/api.js
@@ -7,7 +7,7 @@ const PORT = 3000;
 const nodeAddress = uuid().split('-').join('');
 const bitcoin = new Blockchain();
 app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 
 
 // fetching all the blockchain 
@@ -20,14 +20,14 @@ app.get("/blockchain", function (req, res) {
 app.post("/transaction", function (req, res) {
     // Creating the transaction from the browser
     const blockIndex = bitcoin.createNewTransaction(req.body.amount, req.body.sender, req.body.recipient);
-    res.json({note: `Transaction will be added in block ${blockIndex}. `})
+    res.json({ note: `Transaction will be added in block ${blockIndex}. ` })
 });
 
 // Endppint to mine a new block
 app.get("/mine", function (req, res) {
 
 
-    
+
     // To mine a block we need to create a new block
     // We need to find our params
     // Last Block 
@@ -47,20 +47,20 @@ app.get("/mine", function (req, res) {
 
     // Claculating the Hash for the new block
     const currentBlockHash = bitcoin.hashBlock(previousBlockHash, currentBlockData, nonce)
-    
-     // Creating a new Block to mine the previous one 
-    // const newBlock = bitcoin.createNewBlock(nonce, previousBlockHash, currentBlockHash);
+
+    // Creating a new Block to mine the previous one 
+    const newBlock = bitcoin.createNewBlock(nonce, previousBlockHash, currentBlockHash);
 
     res.json({
-        currentBlockData: currentBlockData,
-        nonce: nonce, 
-        hash: currentBlockHash,
-        previousBlockHash: previousBlockHash,
-        lastBlock: lastBlock,
-        // newBlock: newBlock
+        // currentBlockData: currentBlockData,
+        // nonce: nonce,
+        // hash: currentBlockHash,
+        // previousBlockHash: previousBlockHash,
+        // lastBlock: lastBlock,
+        newBlock: newBlock
     });
 
-   
+
 });
 
 
