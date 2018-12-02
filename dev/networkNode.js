f98305bfee4af2cb12e455a49f30cf69666ad894
willou <walid.boubguira@gmail.com>
2018-12-01 20:05:17 -0500

    Nodes Connected

--- dev/networkNode.js
+++ dev/networkNode.js
@@ -126,9 +126,20 @@ app.post('/register-node', function(req, res){
     });
 });
 
-// register 
+
+// register Bulk network
 app.post('/register-nodes-bulk', function(req, res){
 
+    const allNetworkNodes = req.body.allNetworkNodes;
+    allNetworkNodes.forEach(networkNodeUrl => {
+        const nodeNotAlreadyPresent = bitcoin.networkNodes.indexOf(networkNodeUrl) == -1;
+        const notCurrentNode = bitcoin.currentNodeUrl !== networkNodeUrl;
+        if (nodeNotAlreadyPresent && notCurrentNode) bitcoin.networkNodes.push(networkNodeUrl);
+    });
+
+    res.json({
+        note: "Bulk Successfully Registered."
+    });
 });
 
 
